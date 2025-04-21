c98af5c0e28d59251361cbe8d4dd2f6f1c9c046a
dylann97 <d.xavero@hotmail.com>
2025-04-22 00:54:32 +0200

    🚧 Add new data

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -13,3 +13,256 @@ export const contactInfo = {
   displayPhone: "06 99 09 69 25",
   email: "contact@tcsplomberie.fr",
 };
+
+// Add Review link
+export const reviewLink =
+  "https://www.google.com/search?sca_esv=ceaa0c7f27a790d3&si=APYL9bs7Hg2KMLB-4tSoTdxuOx8BdRvHbByC_AuVpNyh0x2KzQlz25UyBP0snsFPUtdyAwvydqDbK7V5F0nMAUD3OUIKIRBCCat0hhHSUMYBi41D4cFtohgTYhJuAy_zzD-9CIsosRUSfq16jNcWuW6h0TwfAt01hfkk9KX8T7THroXlvlp9ISImNqnvYjxgWH35BOje5CHBPWzydAWbeLndRk0ZFTrZuBbc7_0GBLeZYyt7IPd0OfWDR8SUz6DGDconMPTdmcfnhYFLEG6ITQI-d3yoWrsiLw%3D%3D&q=Thermo+Clim+Sanitaire+-+Plombier+-+Chauffagiste+-+Climatisation+-+Montpellier+-+Installation+-+D%C3%A9pannage+-+Entretien+Avis&sa=X&ved=2ahUKEwjLtPDxiOmMAxUvV6QEHWH4CJwQ0bkNegQINhAD&biw=1800&bih=1004&dpr=2";
+
+// Reviews data
+export const reviews = [
+  {
+    id: "1",
+    name: "Alice Fgr",
+    message:
+      "Merci pour la prestation d'entretien c'est parfait. J'ai retrouvé une bonne ventilation de ma climatisation réversible et un air propre. Bravo pour le professionnalisme, je vous recommande l'entreprise Thermo Clim Sanitaire",
+    stars: 5,
+  },
+  {
+    id: "2",
+    name: "Liza Lenain",
+    message:
+      "Très bon service, rapide et sérieux. J’ai fait refaire ma salle de bain et j’ai fait poser une Clim avec Thermo Clim Sanitaire, à Saint Martin Montpellier. Prix raisonnables. Je recommande 100%",
+    stars: 5,
+  },
+  {
+    id: "3",
+    name: "Suzan Bozkurt",
+    message:
+      "Ce fut un réel plaisir de pouvoir travailler avec Thermo Clim Sanitaire. Très professionnel et efficace. Nous avons fait faire l'installation de notre Cumulus et installation de nos sanitaires. Un travail impeccable propre et soigné. Nous recommandons véritablement son travail.",
+    stars: 5,
+  },
+  {
+    id: "4",
+    name: "Pierre Sargousse",
+    message:
+      "Personne très professionnelle. Le contact par téléphone a été rapide et efficace (par envoi de simples photos), il est arrivé le jour J avec tout le nécessaire. L'intervention a été ensuite très bien réalisée. Je recommande à 100 %",
+    stars: 5,
+  },
+  {
+    id: "5",
+    name: "Réan",
+    message:
+      "Service rapide et efficace, j'ai fait appel l'entreprise Thermo Clim Sanitaire à Montpellier pour un dépannage urgent et il est arrivé rapidement, le technicien a été professionnel et a résolu le problème en peu de temps, je vous recommande fortement leurs services !",
+    stars: 5,
+  },
+  {
+    id: "6",
+    name: "B. Flz",
+    message:
+      "Chez moi à Montpellier J’ai fait installer un ballon d’eau chaude avec l’entreprise Thermo Clim Sanitaire, le travail a été soigné, le rendez-vous a été respecté et le prix était raisonnable, je suis très satisfait de leur service, je recommande !",
+    stars: 5,
+  },
+  {
+    id: "7",
+    name: "Eva Ev",
+    message:
+      "Je recommande fortement cette société si vous recherchez un travail et des produits de qualité. Très professionnel et à l’écoute, nous sommes ravis du travail effectué à plusieurs reprises pour de la plomberie mais aussi pour de la climatisation.",
+    stars: 5,
+  },
+  {
+    id: "8",
+    name: "Vladimir Vladimirov",
+    message:
+      "Ma salle de bain était refaite à la perfection. Je suis très satisfait de la prestation professionnelle effectuée par vos services. Je recommande à tout le monde les yeux fermés. Merci",
+    stars: 5,
+  },
+  {
+    id: "9",
+    name: "Germain Valero",
+    message:
+      "Thermo Clim sanitaire est une entreprise très sérieuse, très bon travail et tient les délais",
+    stars: 5,
+  },
+  {
+    id: "10",
+    name: "Judith Jimenez Cruz",
+    message:
+      "Intervention trés professionnelle et rapide. Tarifs trés raisonnables ! Je vous commande vivement cette entreprise !",
+    stars: 4,
+  },
+  {
+    id: "11",
+    name: "Camille",
+    message:
+      "Professionnel méticuleux et efficace. Merci encore pour le travail accompli dans ma salle de bain et ma cuisine. Je recommande +++",
+    stars: 5,
+  },
+  {
+    id: "12",
+    name: "Maxime Berkpinar",
+    message:
+      "Bon plombier , il a intervenu plusieurs fois pour des travaux de plomberie dans mon restaurant et chez moi 👍",
+    stars: 5,
+  },
+  {
+    id: "13",
+    name: "Erdogan",
+    message:
+      "Très professionnel, à l'écoute du client et surtout travail proprement, je conseille les yeux fermé.",
+    stars: 5,
+  },
+  {
+    id: "14",
+    name: "Pierre Ona",
+    message: "Top plombier réactif efface à appeler sans hésiter je recommande",
+    stars: 5,
+  },
+  {
+    id: "15",
+    name: "Ana Rusnac",
+    message:
+      "le meilleur et le plus responsable artisan Je recommande avec la plus grande confiance !",
+    stars: 5,
+  },
+  {
+    id: "16",
+    name: "El chapo",
+    message: "Franchement bravo très professionnel",
+    stars: 5,
+  },
+  {
+    id: "17",
+    name: "Attila Zac",
+    message:
+      "Dispo , rapide , travail professionnel ! Je vous recommande vivement 👍",
+    stars: 5,
+  },
+  {
+    id: "18",
+    name: "Sandrine Pages",
+    message: "Réactif (rdv très rapide), ponctuel, sérieux!",
+    stars: 5,
+  },
+  {
+    id: "19",
+    name: "Mme Dominique Nurit",
+    message: "Sérieux, efficace et correct en prix. What else!",
+    stars: 5,
+  },
+  {
+    id: "20",
+    name: "Daniel Bozkurt",
+    message: "Efficacité , rapide , travaille propre 👍 merci",
+    stars: 5,
+  },
+  {
+    id: "21",
+    name: "sas FAM",
+    message:
+      "Merci 1000 fois pour la rapidité d'intervention, Service et réparation irréprochable, très sympa ,très bonne communication , je recommande à 100%",
+    stars: 5,
+  },
+  {
+    id: "22",
+    name: "Johannes Adedemi",
+    message:
+      "Merci pour ta réactivité un dimanche de Pâques ! Je recommande à mille pour-cent. Professionnel et très humain. Merci 🙏🏾",
+    stars: 5,
+  },
+  {
+    id: "23",
+    name: "Sena Christelle Hountondji",
+    message:
+      "Super réactif, humain.. il s’est déplacé un dimanche pour régler notre soucis de plomberie. Merci encore à vous 🙏",
+    stars: 5,
+  },
+  {
+    id: "24",
+    name: "Suleyman Seker",
+    message:
+      "Plombier de confiance, très bonne communication et très réactif. Nous sommes très satisfait du remplacement de notre ballon d’eau chaude,ils nous ont établis un devis très rapidement et sont intervenus très rapidement également sachant que nous n’avions plus d’eau chaude, je vous recommande cette société Thermo Clim Sanitaire",
+    stars: 5,
+  },
+];
+
+export const faqData = [
+  {
+    id: "item-1",
+    question: "En combien de temps pouvez-vous intervenir à Montpellier ?",
+    answer:
+      "Nous intervenons généralement dans la journée pour toute urgence plomberie, chauffage ou climatisation à Montpellier et ses alentours. Pour une installation planifiée, nous vous proposons un rendez-vous rapide selon vos disponibilités.",
+  },
+  {
+    id: "item-2",
+    question: "Intervenez-vous en dehors de Montpellier ?",
+    answer:
+      "Oui, nous nous déplaçons dans toute la métropole de Montpellier, notamment à : Castelnau-le-Lez, Lattes, Pérols, Mauguio, Clapiers, Jacou, Grabels, Juvignac, Le Crès, Saint-Jean-de-Védas, Saint-Gély-du-Fesc, Teyran, Prades-le-Lez, Fabrègues, Castries et Sète. N'hésitez pas à nous contacter pour vérifier si nous couvrons votre commune.",
+  },
+  {
+    id: "item-3",
+    question: "Proposez-vous des devis gratuits ?",
+    answer:
+      "Oui, tous nos devis sont gratuits et sans engagement. Que ce soit pour un dépannage ou une installation, nous vous envoyons un devis clair avant chaque intervention.",
+  },
+  {
+    id: "item-4",
+    question: "Quels types de travaux de plomberie réalisez-vous ?",
+    answer:
+      "Nous prenons en charge tous vos besoins : fuites d’eau, débouchage de canalisation, remplacement de robinet, installation de sanitaires, chauffe-eau, etc. Nous assurons également l’entretien régulier de vos équipements.",
+  },
+  {
+    id: "item-5",
+    question: "Entretenez-vous les chaudières et les climatisations ?",
+    answer:
+      "Oui, nous assurons l’entretien et le dépannage de chaudières gaz, électriques, et de systèmes de climatisation. Un entretien régulier garantit la sécurité et la longévité de vos équipements.",
+  },
+  {
+    id: "item-6",
+    question: "Êtes-vous certifié ou assuré pour vos interventions ?",
+    answer:
+      "Oui, nous sommes assurés et déclarés. Vous bénéficiez d’une intervention réalisée dans le respect des normes, avec une garantie sur les travaux effectués. La sécurité et la transparence sont nos priorités.",
+  },
+];
+
+export const services = [
+  { id: "plomberie", label: "Plomberie" },
+  { id: "chauffage", label: "Chauffage" },
+  { id: "climatisation", label: "Climatisation" },
+];
+
+export const cities = [
+  "Montpellier",
+  "Lattes",
+  "Pérols",
+  "Mauguio",
+  "Castelnau-le-Lez",
+  "Clapiers",
+  "Fabrègues",
+  "Grabels",
+  "Jacou",
+  "Juvignac",
+  "Le Crès",
+  "Prades-le-Lez",
+  "Saint-Gély-du-Fesc",
+  "Saint-Jean-de-Védas",
+  "Villeneuve-lès-Maguelone",
+  "Teyran",
+  "Castries",
+  "Sète",
+  "La Grande-Motte",
+  "Grau-du-Roi",
+  "Aigues-Mortes",
+  "Baillargues",
+  "Vendargues",
+  "Frontignan",
+  "Lavérune",
+  "Saint-Aunès",
+  "Palavas-les-Flots",
+  "Pignan",
+  "Saint-Georges-d'Orques",
+  "Saussan",
+  "Saint-Brès",
+  "Mudaison",
+  "Lansargues",
+  "Valergues",
+];
