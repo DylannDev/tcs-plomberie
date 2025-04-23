25bd20ef0fcf1479a32b7ad84e572a27e8d2b3a3
dylann97 <d.xavero@hotmail.com>
2025-04-23 23:20:42 +0200

    🚧 Build meta component for seo

---
+++ src/components/ui/meta-head.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+interface MetaHeadProps {
+  title: string;
+  description: string;
+}
+
+export function MetaHead({ title, description }: MetaHeadProps) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+    </Head>
+  );
+}
