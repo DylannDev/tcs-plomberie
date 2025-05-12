03bffe4aa464a58fb803e83fe91817f6c6d1fbaf
dylann97 <d.xavero@hotmail.com>
2025-05-12 18:06:00 +0200

    fix bad animation from framer

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -7,69 +7,67 @@ import { ButtonCall } from "../ui/button-call";
 import { ButtonQuote } from "../ui/button-quote";
 import { FlipWords } from "../ui/flip-words";
 import { seoWords } from "@/src/data";
+import { AnimatedHeader } from "../ui/animated-header";
 
 export function Hero() {
   return (
-    <section className="relative overflow-hidden max-h-[800px] hidden md:flex justify-center w-full mx-auto px-0 md:px-6 lg:px-8 max-w-[1440px]">
-      <div className="relative w-full">
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-          className="text-center"
-        >
-          <div className="relative min-h-[calc(100dvh-218px)] flex w-full rounded-4xl overflow-hidden">
-            <Image
-              src={"/shape-1.svg"}
-              alt="Plombier Montpellier TCS Plomberie"
-              width={600}
-              height={600}
-              quality={100}
-              className="absolute -top-28 -left-36 scale-x-[-1] z-49 overflow-hidden"
-            />
-            <div className="absolute top-0 bottom-0 bg-linear-to-r from-white via-white/70 to-white/0 w-full h-full z-40 rounded-4xl overflow-hidden"></div>
-            <div className="relative w-full max-h-[800px] aspect-square md:aspect-video rounded-4xl overflow-hidden">
+    <AnimatedHeader>
+      <section className="relative overflow-hidden max-h-[800px] hidden md:flex justify-center w-full mx-auto px-0 md:px-6 lg:px-8 max-w-[1440px]">
+        <div className="relative w-full">
+          <div className="text-center">
+            <div className="relative min-h-[calc(100dvh-218px)] flex w-full rounded-4xl overflow-hidden">
               <Image
-                src={"/plombier-5.jpg"}
+                src={"/shape-1.svg"}
                 alt="Plombier Montpellier TCS Plomberie"
-                fill
-                sizes="100vw"
+                width={600}
+                height={600}
                 quality={100}
-                className="object-cover object-center"
-                priority
+                className="absolute -top-28 -left-36 scale-x-[-1] z-49 overflow-hidden"
               />
+              <div className="absolute top-0 bottom-0 bg-linear-to-r from-white via-white/70 to-white/0 w-full h-full z-40 rounded-4xl overflow-hidden"></div>
+              <div className="relative w-full max-h-[800px] aspect-square md:aspect-video rounded-4xl overflow-hidden">
+                <Image
+                  src={"/plombier-5.jpg"}
+                  alt="Plombier Montpellier TCS Plomberie"
+                  fill
+                  sizes="100vw"
+                  quality={100}
+                  className="object-cover object-center"
+                  priority
+                />
+              </div>
             </div>
-          </div>
-          <div className="absolute top-0 w-full h-full z-50 flex items-center justify-center">
-            <div className="max-w-3xl px-4">
-              <Typography
-                as="h1"
-                weight="bold"
-                className="text-5xl lg:text-6xl leading-tight text-black relative flex flex-wrap items-center justify-center gap-x-2 md:gap-x-3 mb-6"
-              >
-                <span className="whitespace-nowrap">Besoin d'un</span>{" "}
-                <FlipWords words={seoWords} />
-                <span className="sr-only">
-                  Plomberie, Plombier, Chauffagiste, Climaticien, Climatiseur
-                </span>
-                <span>à Montpellier ?</span>
-              </Typography>
-              <Typography
-                as="p"
-                weight="medium"
-                className="mb-10 text-dark-gray text-base sm:text-2xl text-balance"
-              >
-                TCS Plomberie intervient 7j/7 pour la plomberie, le chauffage et
-                la climatisation à Montpellier et ses alentours.
-              </Typography>
-              <div className="flex flex-col sm:flex-row sm:gap-4 gap-3 justify-center">
-                <ButtonCall />
-                <ButtonQuote color="black" />
+            <div className="absolute top-0 w-full h-full z-50 flex items-center justify-center">
+              <div className="max-w-3xl px-4">
+                <Typography
+                  as="h1"
+                  weight="bold"
+                  className="text-5xl lg:text-6xl leading-tight text-black relative flex flex-wrap items-center justify-center gap-x-2 md:gap-x-3 mb-6"
+                >
+                  <span className="whitespace-nowrap">Besoin d'un</span>{" "}
+                  <FlipWords words={seoWords} />
+                  <span className="sr-only">
+                    Plomberie, Plombier, Chauffagiste, Climaticien, Climatiseur
+                  </span>
+                  <span>à Montpellier ?</span>
+                </Typography>
+                <Typography
+                  as="p"
+                  weight="medium"
+                  className="mb-10 text-dark-gray text-base sm:text-2xl text-balance"
+                >
+                  TCS Plomberie intervient 7j/7 pour la plomberie, le chauffage
+                  et la climatisation à Montpellier et ses alentours.
+                </Typography>
+                <div className="flex flex-col sm:flex-row sm:gap-4 gap-3 justify-center">
+                  <ButtonCall />
+                  <ButtonQuote color="black" />
+                </div>
               </div>
             </div>
           </div>
-        </motion.div>
-      </div>
-    </section>
+        </div>
+      </section>
+    </AnimatedHeader>
   );
 }
