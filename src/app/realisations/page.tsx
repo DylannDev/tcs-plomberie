29e2bbd5522330c11ccb3a44c455068cb2f142e6
dylann97 <d.xavero@hotmail.com>
2025-05-12 13:25:43 +0200

    Delete useless route

--- src/app/realisations/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function RealisationsPage() {
-  return (
-    <div className="container py-8">
-      <h1 className="text-3xl font-bold mb-4">Réalisations</h1>
-      <p>Page en cours de construction</p>
-    </div>
-  );
-}
