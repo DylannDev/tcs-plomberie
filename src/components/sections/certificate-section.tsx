c148d9c49b608b0eaae04aa85de56c3d659326ae
dylann97 <d.xavero@hotmail.com>
2025-05-15 22:08:32 +0200

    🚧 Create new certificate section to improve trust

---
+++ src/components/sections/certificate-section.tsx
@@ -0,0 +1,97 @@
+import Image from "next/image";
+import { Typography } from "@/src/components/ui/typography";
+import { AnimatedCard } from "@/src/components/ui/animated-card";
+import { certificateInfos } from "@/src/data/certificateData";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+
+interface CertificateInfo {
+  badge: string;
+  title: string;
+  image: string;
+  content: string;
+  isButton: boolean;
+  bgColor: string;
+  imgClassName: string;
+}
+
+export function Certificate() {
+  return (
+    <section className="space-y-16 sm:space-y-24 h-full mb-20 sm:my-20">
+      {certificateInfos.map((section: CertificateInfo, index: number) => (
+        <AnimatedCard
+          key={section.title}
+          index={index}
+          className={`${section.bgColor} rounded-3xl py-10 sm:py-16 px-5 sm:px-10`}
+        >
+          <div
+            className={`flex flex-col-reverse gap-8 lg:gap-12 items-center ${
+              index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
+            }`}
+          >
+            {/* Image */}
+            <div className="w-full aspect-square lg:w-1/2 flex justify-center items-center">
+              <div
+                className={`relative aspect-square w-full rounded-3xl ${section.imgClassName}`}
+              >
+                <Image
+                  src={section.image}
+                  alt={section.title}
+                  fill
+                  className="absolute object-contain rounded-3xl"
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  priority={index === 0}
+                />
+              </div>
+            </div>
+
+            {/* Content */}
+            <div className="w-full lg:w-1/2 space-y-4">
+              <div className="text-center sm:text-left mb-10">
+                <Badge variant={index === 1 ? "dark" : "light"}>
+                  {section.badge}
+                </Badge>
+                <Typography
+                  as="h2"
+                  variant="3xl"
+                  weight="bold"
+                  className="text-balance"
+                >
+                  {section.title}
+                </Typography>
+              </div>
+              <div
+                className="rich-text "
+                dangerouslySetInnerHTML={{ __html: section.content }}
+              />
+              {section.isButton && (
+                <div className="flex flex-col gap-4 mt-10">
+                  <Button variant="outline">
+                    <a
+                      href="/flyer-qualipac.pdf"
+                      target="_blank"
+                      className="text-base sm:text-lg"
+                    >
+                      Télécharger le guide QualiPac (PDF)
+                    </a>
+                  </Button>
+
+                  <Button>
+                    <a
+                      href="https://www.qualit-enr.org/entreprises/thermo-clim-sanitaire/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-base sm:text-lg"
+                    >
+                      Voir notre fiche officielle QualiPac
+                    </a>
+                  </Button>
+                </div>
+              )}
+            </div>
+          </div>
+        </AnimatedCard>
+      ))}
+    </section>
+  );
+}
---
+++ src/data/certificateData.tsx
@@ -0,0 +1,34 @@
+export const certificateInfos = [
+  {
+    badge: "QualiPac",
+    title:
+      "Certification QualiPac – Un gage de qualité et d’efficacité énergétique",
+    image: "/rge-qualipac-logo.png",
+    content: `
+      <p>Thermo Clim Sanitaire Plomberie est une entreprise spécialisée dans l’installation de systèmes de climatisation et de chauffe-eaux thermodynamiques à Montpellier et ses alentours.</p>
+      <p>En tant qu’installateur certifié QualiPac, nous vous garantissons une expertise reconnue dans la pose de pompes à chaleur aérothermiques et géothermiques, en conformité avec les normes RGE (Reconnu Garant de l’Environnement).</p>
+      <p>Cette certification vous permet également de bénéficier des aides financières de l’État pour vos travaux d’amélioration énergétique (MaPrimeRénov’, primes CEE, etc.).</p>
+      <p>Soucieux de l’environnement, nous vous accompagnons vers des solutions de chauffage et de climatisation écologiques, performantes et économiques, parfaitement adaptées à vos besoins.</p>
+      <p>Vous êtes à la recherche d’un artisan RGE à Montpellier pour l’installation d’une pompe à chaleur ou d’un chauffe-eau thermodynamique ? Faites appel à Thermo Clim Sanitaire pour un devis gratuit et une intervention rapide 7j/7.</p>
+    `,
+    isButton: true,
+    bgColor: "bg-light-gray-2",
+    imgClassName: "",
+  },
+  {
+    badge: "Assurance décennale",
+    title: "Assurance décennale – Votre tranquillité est garantie",
+    image: "/decenale.webp",
+    content: `
+      <p>Chez TCS Plomberie, nous nous engageons à réaliser vos travaux de plomberie, chauffage et climatisation dans le respect total des normes de qualité et de sécurité.</p>
+      <p>C’est pourquoi nous sommes couverts par une <strong>assurance décennale professionnelle</strong>, vous garantissant une protection complète pendant 10 ans après l’installation ou les travaux réalisés à votre domicile.</p>
+      <p>Cette garantie couvre les éventuels dommages qui pourraient compromettre la solidité de l’ouvrage ou le rendre impropre à son usage, qu’il s’agisse de l'installation d’une pompe à chaleur, d’un chauffe-eau thermodynamique, ou de travaux de plomberie importants.</p>
+      <p>En tant qu’artisan déclaré, assuré et expérimenté, nous mettons un point d'honneur à travailler avec sérieux, rigueur et transparence pour la satisfaction de chaque client.</p>
+      <p>Avec Thermo Clim Sanitaire Plomberie, vous avez l’assurance de confier vos travaux à un professionnel fiable, reconnu et parfaitement assuré.</p>
+    `,
+    isButton: false,
+    bgColor: "bg-light-blue",
+    imgClassName:
+      "max-h-[350px] max-w-[350px] sm:max-h-[500px] sm:max-w-[500px]",
+  },
+];
